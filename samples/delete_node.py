7471dccb612793961ef5f8a94d9ff055615b0981
sgtp <3esk6j35#ESK^J#%>
2017-09-18 19:10:09 -0400

    Parameterize address in SWQL

--- samples/delete_node.py
+++ samples/delete_node.py
@@ -3,25 +3,26 @@ from orionsdk import SwisClient
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 # setup swis params
-npm_server = '127.0.0.1'
+npm_server = 'localhost'
 username = 'admin'
 password = ''
 
 # disable SSL warnings
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-# Setup the connection
+# Setup the swis connection
 swis = SwisClient(npm_server, username, password)
 
 # Find the Uri you want to delete based on a SWQL query
-results = swis.query("SELECT IPAddress, Caption, Uri FROM Orion.Nodes WHERE IPAddress LIKE '127.0.0.6'")
+results = swis.query("SELECT IPAddress, Caption, Uri FROM Orion.Nodes WHERE IPAddress = @ip_addr", ip_addr='127.0.0.6')
 
 # Use as needed
 if len(results['results']) > 1:
-    print('refine your search found more than one node matching that criteria')
-    [print("{Caption} - {IPAddress} - {Uri}".format(**row)) for row in results['results']]
+    print('refine your search. Found more than one node matching that criteria.')
 elif len(results['results']) == 1:
+    print("Deleting {}".format(results['results'][0]['IPAddress']))
     response = swis.delete(results['results'][0]['Uri'])
-    print("Deleted Node")
+    print("Done")
+
 else:
     print("Nothing to delete")
