4d5cdf81ef02f84143addd1cf1175b5f80188280
Superadm1n <kowalkyl@gmail.com>
2019-05-15 11:31:24 -0500

    Change swisClient to utilize variables

--- samples/suppress_node_alerts.py
+++ samples/suppress_node_alerts.py
@@ -7,7 +7,7 @@ password = 'password'
 ip_address = '192.168.1.1'  # change this to the node you want to use
 
 
-sw = orionsdk.SwisClient('server', 'username', 'password')
+sw = orionsdk.SwisClient(server, username, password)
 uri = sw.query("SELECT Uri FROM Orion.Nodes WHERE NodeID=@node_id", IPAddress=ip_address)['results'][0]['Uri']
 
 # to specify an end time change None to a datetime object
